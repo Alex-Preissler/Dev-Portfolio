cea4874568484cc292674e2cf5c3943b7458c4e5
Alex-Preissler <AlexJPreissler@Outlook.com>
2019-01-12 14:55:21 -0500

    portfolio

---
+++ js/config.js
@@ -0,0 +1,103 @@
+/* Google MAP Settings*/
+  var lat= 23.73176;  //Change the value with your address Latitude
+  var lng= 90.40640;  //Change the value with your address Longitude
+  
+//Skill Chart Data
+var skillData = {
+	skill1: [
+		{
+			title: "Gimp 50%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '50%'
+		},
+		{
+			title: "UI/UX Design 90%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '90%'
+		},
+		{
+			title: "Webtypography 70%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '70%'
+		},
+		{
+			title: "Logo Design 60%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '60%'
+		},
+    {
+			title: "Blender 50%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '50%'
+		}
+	],
+	skill2: [
+		{
+			title: "HTML5/CSS3 90%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '90%'
+		},
+		{
+			title: "JS/Jquery 95%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '95%'
+		},
+		{
+			title: "ReactJS 70%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '70%'
+		},
+		{
+			title: "MySQL 75%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '75%'
+		},
+    {
+			title: "MongoDB 85%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '85%'
+		}
+	],
+  skill3: [
+		{
+			title: "Teamwork 99%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '99%'
+		},
+		{
+			title: "Problem Solving 95%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '95%'
+		},
+    {
+			title: "Time Management 90%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '90%'
+		},
+		{
+			title: "Leadership 90%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '90%'
+		},
+		{
+			title: "Project Management 80%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '80%'
+		}
+	]
+}
